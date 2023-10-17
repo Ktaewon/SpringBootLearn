4a27a8ea93e1e2d8e3d5c2a88f493380a7fca779
Ktaewon <dydalehdxo@naver.com>
2023-10-18 02:32:11 +0900

    [2023/10/18] first commit

---
+++ src/test/java/com/taewon/helloboot/HellobootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taewon.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
